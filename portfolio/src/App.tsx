51758f49ed253d1c9eae88705ee9696c79f4057b
Lucas Bielefeldt <lucasbielefeldt@gmail.com>
2022-07-26 21:08:12 -0300

    import home component and updated div

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,17 @@
 import styles from './App.module.css'
 import { SideBar } from './components/SideBar';
+import { Home } from './components/Home';
 
 export function App() {
 
   return (
+    <div className={styles.container}>
     <div className={styles.nav}>
       <SideBar />
+    </div>
+    <div className="content">
+    <Home />
+    </div>
     </div>
   )
 }
